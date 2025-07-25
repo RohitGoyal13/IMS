1602dc7320e0747906a16d475d142e8ac05a651e
Rohit Goyal <rohitgoyal4220@gmail.com>
2025-07-25 13:56:54 +0530

    - Added Express server configuration in server.js - Connected to MongoDB database - Configured nodemon for auto-restart on changes

--- server.js
+++ server.js
@@ -4,3 +4,18 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './config/db.js';
 
+dotenv.config();
+
+const app = express();
+app.use(express.json()); 
+app.use(cors());
+
+app.get('/' , (req, res) => {
+    res.send('API is running...');
+});
+
+const PORT = process.env.PORT || 5000;
+
+connectDB().then(() => {
+    app.listen(PORT, () => console.log('Server is running on port', PORT));
+});
